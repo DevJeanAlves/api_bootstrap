2cc8101cc50006a11ace1ec8f3918a78d423c018
JeanAlves2021 <jean.pdolsky2@gmail.com>
2022-02-01 01:01:34 -0300

    studing EJS

--- server.js
+++ server.js
@@ -4,7 +4,36 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('pages/index')
+  const items = [
+    {
+      title: 'D',
+      message: 'esenvolver aplicações/serviçoes de forma fácil.'
+    },
+    {
+      title: 'E',
+      message: 'JS utiliza Javascript para renderizar HTML.'
+    },
+    {
+      title: 'M',
+      message: 'uito fácil de usar.'
+    },
+    {
+      title: 'A',
+      message: 'morzinho.'
+    },
+    {
+      title: 'I',
+      message: 'nstall EJS.'
+    },
+    {
+      title: 'S',
+      message: 'intaxe simples.'
+    }
+  ]
+
+  const subtitle = 'Uma linguagem de modelagem para criação de paginas HTML.'
+
+  res.render('pages/index', { qualities: items, subtitle: subtitle })
 })
 
 app.get('/about', function (req, res) {
